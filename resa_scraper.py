870382d8a804702bd8f4b12c7901eb75c21631ec
Miavaka <miavaka80@gmail.com>
2026-01-19 13:28:45 +0300

    NO USAGE PROFIL mais Ajustement création driver

--- resa_scraper.py
+++ resa_scraper.py
@@ -74,9 +74,9 @@ class resa_scraper(object):
         #pour le profil (à fin de stocker les cahes et cookies) #16 01 2026
         # self.chrome_options.add_argument("--no-default-browser-check") # Évite la vérification du navigateur par défaut
         # self.chrome_options.add_argument("--disable-sync")
-        profil = self.name_of_destination_file.replace('resanc_dest','')
-        print(f'Dossier de profil utilisé pour cette instance => profil{profil}')
-        self.chrome_options.add_argument(f"--user-data-dir={PROFILE_CHROME}/profil{profil}")
+        # profil = self.name_of_destination_file.replace('resanc_dest','')
+        # print(f'Dossier de profil utilisé pour cette instance => profil{profil}')
+        # self.chrome_options.add_argument(f"--user-data-dir={PROFILE_CHROME}/profil{profil}")
 
         self.data_container = []
         self.count_url_no_price = 0
