b38e5d695ed0c6b64009130d369ec9e5cdb5b481
Miavaka <miavaka80@gmail.com>
2026-02-17 17:03:20 +0300

    test variable sur serveur : test de refresh après erreur dans big container

--- resa_scraper.py
+++ resa_scraper.py
@@ -574,10 +574,12 @@ class resa_scraper(object):
                             print('refresh de la page car error module does not recognize this error rencontré')
                             self.driver.refresh() #les dates en paramètres sont retenus j'ai regardé et vérifier, le currency XPF aussi
                             time.sleep(randint(2,3))
+                            self.driver.refresh()#une deuxième fois car parfois la deuxième fois ça marche
+                            time.sleep(randint(2,3))
                             #on recheck les variables car ils sont perdu d'après mes tests mais pas les selections sur la page
                             soup = BeautifulSoup(self.driver.page_source, "html.parser") #lxml ne fonctionne pas sur serveur
                             big_container_offer = soup.find('div', {'class':'offer-list offer-list0 last m-top-30'})
-                            input(f'APRES REFRESH , BIG CONTAINER OFFER = {big_container_offer}')
+                            # input(f'APRES REFRESH , BIG CONTAINER OFFER = {big_container_offer}')
                             if big_container_offer is None:
                                 no_reservable = soup.find("div", string=re.compile("la durée minimum", re.IGNORECASE)) #exemple de message : "la durée minimum pour réserver cet hébergement est de 3 nuits"
                                 if no_reservable != None:
