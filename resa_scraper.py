ba39152d55bcc7daa0342043de76e551af40ee58
Miavaka <miavaka80@gmail.com>
2026-02-24 18:37:04 +0300

    FIX Suite optimisation : Ajout refresh deux fois + rappel de la méthode en question,dans lequel l'erreur not recognize s'st produite

--- resa_scraper.py
+++ resa_scraper.py
@@ -571,13 +571,13 @@ class resa_scraper(object):
                         print(f'No reservable not found and == None => {no_reservable}')
                         soup = BeautifulSoup(self.driver.page_source, "html.parser")
                         print(f'Voici ce qui est affiché dans le body au moment de l\'erreur -> {soup.find("body").get_text(strip=True)}') #mà on voit mieux si c'est du module not recognize
-                        if soup.find("body").get_text(strip=True) == "The custom error module does not recognize this error.":
+                        if soup.find("body").get_text(strip=True) == "The custom error module does not recognize this error." or soup.find("body").get_text(strip=True):
                             self.driver.refresh()
                             time.sleep(1)
                             self.driver.refresh()
                             time.sleep(1)
                             input('Regarde le navigateur si les filtres sont tous OK après actualisation')
-                            self.extract_data(check_dispo, nom, localite, datas, index_for_datas)
+                            self.extract_in_eloha(check_dispo, nom, localite, datas, index_for_datas)
                         else:
                             sys.exit('STOP , CHECK NAVIGATEUR si non fermé') 
                 elif big_container_offer:
@@ -586,13 +586,13 @@ class resa_scraper(object):
                 print(f'Selecteur big container offer not found -> {e}, CHECK')
                 soup = BeautifulSoup(self.driver.page_source, "html.parser")
                 print(f'Voici ce qui est affiché dans le body au moment de l\'erreur -> {soup.find("body").get_text(strip=True)}') #mà on voit mieux si c'est du module not recognize
-                if soup.find("body").get_text(strip=True) == "The custom error module does not recognize this error.":
+                if soup.find("body").get_text(strip=True) == "The custom error module does not recognize this error." or soup.find("body").get_text(strip=True):
                     self.driver.refresh()
                     time.sleep(1)
                     self.driver.refresh()
                     time.sleep(1)
                     input('Regarde le navigateur si les filtres sont tous OK après actualisation')
-                    self.extract_data(check_dispo, nom, localite, datas, index_for_datas)
+                    self.extract_in_eloha(check_dispo, nom, localite, datas, index_for_datas)
                 else:
                     sys.exit('STOP , CHECK NAVIGATEUR si non fermé') 
 
